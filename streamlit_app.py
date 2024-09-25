f2e9c88692ef44324aad42f5636cd457b7b0877d
goldsweetpotato <56987753+goldsweetpotato@users.noreply.github.com>
2024-09-25 17:13:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
-
+from transformers import pipeline
+st.title("My AI Assistant")
 user_prompt=st.text_input("user's prompt")
 token_length=st.text_input("token length")
+generator=pipeline('text-generation', model= "gpt2")
+prompt = user_prompt
+output= generator(promot, max_length=token_length, num_return_squences=10, truncation=true)[0]
+st.write(output("generated_text")
