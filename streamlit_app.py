d20016f2b8fa2f8f0a6309e5ecba915ea4aca856
goldsweetpotato <56987753+goldsweetpotato@users.noreply.github.com>
2024-09-25 17:20:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ st.title("My AI Assistant")
 user_prompt=st.text_input("user's prompt")
 token_length=st.text_input("token length")
 generator=pipeline('text-generation', model= "gpt2")
-prompt = user_prompt
-output= generator(promot, max_length=token_length, num_return_squences=10, truncation=true)[0]
+prompt = st.text_input()
+output= generator(prompt, max_length=token_length, num_return_squences=10, truncation=true)[0]
 st.write(output("generated_text"))
